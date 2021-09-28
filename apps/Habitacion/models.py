80c81a5e966c423830487cc3066f136db9c1d398
Tulio Rafael Cuadrado Hoyos <tcuadrado@uniguajira.edu.co>
2021-09-27 20:37:56 -0500

    Tulio Cuadrado Hoyos

--- apps/Habitacion/models.py
+++ apps/Habitacion/models.py
@@ -10,4 +10,10 @@ class Habitacion(models.Model):
 
     Tipo_De_Habitaciones = models.CharField(max_length=45)
     Hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+
+
+
+    def __str__(self):
+        return self.Tipo_De_Habitaciones
     
+
