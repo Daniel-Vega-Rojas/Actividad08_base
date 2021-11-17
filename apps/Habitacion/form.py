3938fbeabd881b7395da18eef2aad89e7afceb07
Daniel Vega <djosevega@uniguajira.edu.co>
2021-11-16 23:43:08 -0500

    Daniel vega con ayuda de deider blanco

--- apps/Habitacion/form.py
+++ apps/Habitacion/form.py
@@ -9,15 +9,18 @@ class HabitacionForm(forms.ModelForm):
         model = Habitacion
 
         fields = [
-            'Tipo_De_Habitaciones'
+            'Tipo_De_Habitaciones',
+            'Hotel'
         ]
 
         Labels = {
-            'Tipo_De_Habitaciones': 'Ingrese Nombre del tipo de habitacion'
+            'Tipo_De_Habitaciones': 'Ingrese Nombre del tipo de habitacion',
+            'Hotel': "hotel"
         }
 
         widgets = {
-            'Tipo_De_Habitaciones':forms.TextInput(attrs={'class': 'form-control'})
+            'Tipo_De_Habitaciones':forms.TextInput(attrs={'class': 'form-control'}),
+            'Hotel' :forms.Select(attrs={'class': 'form-control'})
 
             # en el caso de que haya relaciones, se desglose un espacio para seleccionar
             # 'tipoClient' :forms.Select(attrs={'class': 'form-control'}),
